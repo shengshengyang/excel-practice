e886465e0f11f1fd11890766a547fd958fa2014e
deanyang122 <i64991189@gs.ncku.edu.tw>
2023-03-09 17:25:07 +0800

    Modify: 新增圖片大小取得及限制

--- src/main/java/org/example/ExcelImage.java
+++ src/main/java/org/example/ExcelImage.java
@@ -9,6 +9,10 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -28,10 +32,20 @@ public class ExcelImage {
             // Load the logo image file
             byte[] logoBytes = IOUtils.toByteArray(new FileInputStream("psvm.png"));
 
+            // Get the size of the image
+            Dimension imageSize = getImageSize(logoBytes);
+
+
+            // Set a maximum size limit of 10 blocks
+            int maxBlockSize = 10;
+            int width = Math.min(maxBlockSize, imageSize.width);
+            int height = Math.min(maxBlockSize+10, imageSize.height);
+
+
             // Add the logo to the sheet
             int logoIndex = workbook.addPicture(logoBytes, Workbook.PICTURE_TYPE_PNG);
             Drawing<?> drawing = sheet.createDrawingPatriarch();
-            ClientAnchor anchor = new XSSFClientAnchor(0, 0, 0, 0, 0, 0, 1, 1);
+            ClientAnchor anchor = new XSSFClientAnchor(0, 0, 0, 0, 0, 0, width, height);
             drawing.createPicture(anchor, logoIndex);
 
             // Write the workbook to a file
@@ -44,5 +58,11 @@ public class ExcelImage {
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    private static Dimension getImageSize(byte[] imageData) throws IOException {
+        // Use ImageIO to get the size of the image
+        BufferedImage image = ImageIO.read(new ByteArrayInputStream(imageData));
+        return new Dimension(image.getWidth(), image.getHeight());
     }
 }
