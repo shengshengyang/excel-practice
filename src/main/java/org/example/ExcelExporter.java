fd04b6f0fa8a346a32ac9ff9d03773b363745b93
deanyang122 <i64991189@gs.ncku.edu.tw>
2023-03-09 16:58:28 +0800

    Modify: 新增reader

--- src/main/java/org/example/ExcelExporter.java
+++ src/main/java/org/example/ExcelExporter.java
@@ -1,8 +1,56 @@
 package org.example;
 
-public class ExcelExporter {
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class ExcelExporter {
     public static void main(String[] args) {
-        
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("My Sheet");
+
+        // Create some sample data
+        Row row1 = sheet.createRow(0);
+        row1.createCell(0).setCellValue("Name");
+        row1.createCell(1).setCellValue("Age");
+
+        Row row2 = sheet.createRow(1);
+        row2.createCell(0).setCellValue("John");
+        row2.createCell(1).setCellValue(30);
+
+        Row row3 = sheet.createRow(2);
+        row3.createCell(0).setCellValue("Jane");
+        row3.createCell(1).setCellValue(25);
+
+        if (sheet.getPhysicalNumberOfRows() > 0) {
+            Row row = sheet.getRow(sheet.getFirstRowNum());
+            Iterator<Cell> cellIterator = row.cellIterator();
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                int columnIndex = cell.getColumnIndex();
+                sheet.autoSizeColumn(columnIndex);
+                int currentColumnWidth = sheet.getColumnWidth(columnIndex);
+                try {
+                    sheet.setColumnWidth(columnIndex, (currentColumnWidth + 500));
+                } catch (IllegalArgumentException iae) {
+                    sheet.setColumnWidth(columnIndex, currentColumnWidth);
+                }
+            }
+        }
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream("workbook.xlsx");
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/org/example/ExcelReader.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class ExcelReader {
+
+    public static void main(String[] args) {
+
+        try {
+            // Load the Excel file
+            File file = new File("workbook.xlsx");
+            FileInputStream fis = new FileInputStream(file);
+            XSSFWorkbook workbook = new XSSFWorkbook(fis);
+            Sheet sheet = workbook.getSheetAt(0);
+
+            // Iterate over each row in the sheet starting from the second row
+            for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+                Row row = sheet.getRow(rowIndex);
+
+                if (row != null) {
+                    Cell cell1 = row.getCell(0);
+                    Cell cell2 = row.getCell(1);
+
+                    if (cell1 != null && cell2 != null) {
+                        String name = cell1.getStringCellValue();
+                        int age = (int) cell2.getNumericCellValue();
+
+                        System.out.println("Name: " + name + ", Age: " + age);
+                    }
+                }
+            }
+
+            // Close the input stream and workbook
+            fis.close();
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
